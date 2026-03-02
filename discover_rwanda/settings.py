da1c00419da9be238aa2aa1bbe97357537ab12ca
Mc-Knight <tetobobo1@gmail.com>
2026-03-02 17:50:41 +0200

    update CORS and CSRF trusted origins for production deployment

--- discover_rwanda/settings.py
+++ discover_rwanda/settings.py
@@ -137,11 +137,27 @@ ADMIN_EMAIL = "dvdnnkr@gmail.com"  # where notifications are sent
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5000",
+    "http://127.0.0.0",
+    "http://197.243.57.75",
+    "discoverrwanda.rw",
+    "www.discoverrwanda.rw",
+    "https://discoverrwanda.rw",
+    "https://www.discoverrwanda.rw",
+    "http://discoverrwanda.rw",
+    "http://www.discoverrwanda.rw",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5000",
+    "http://127.0.0.0:8000",
+    "http://197.243.57.75",
+    "discoverrwanda.rw",
+    "www.discoverrwanda.rw",
+    "https://discoverrwanda.rw",
+    "https://www.discoverrwanda.rw",
+    "http://discoverrwanda.rw",
+    "http://www.discoverrwanda.rw",
 ]
 
 
